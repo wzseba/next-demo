86ca2ffafba7b71d1be4127614d35b3d5f206038
wzseba <wzseba@gmail.com>
2023-07-03 18:14:13 -0300

    first commit

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,23 +5,23 @@ import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function About() {
   return (
     <>
       <Head>
-        <title>Home Sebastian</title>
-        <meta name="description" content="Home page" />
+        <title>About Page</title>
+        <meta name="description" content="About page" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <h1>
-            Ir a <a href='/about'>About</a>
+            Ir a <a href='/'>Home</a>
           </h1>
           <p>
             Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
+            <code className={styles.code}>pages/about.tsx</code>
           </p>
         </div>
       </main>
