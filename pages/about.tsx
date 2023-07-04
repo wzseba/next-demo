081a572fae88be02ba6acc30d676ccd760336892
wzseba <wzseba@gmail.com>
2023-07-03 21:33:48 -0300

    nueva pagina

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
+
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
+import { Navbar } from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,10 +16,11 @@ export default function About() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar/>
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <h1>
-            Ir a <a href='/'>Home</a>
+            Ir a <Link href='/'>Home</Link>
           </h1>
           <p>
             Get started by editing&nbsp;
