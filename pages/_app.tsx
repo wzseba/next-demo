8dfe44cfaf346b93f76aa62f793568bf9f4ce560
wzseba <wzseba@gmail.com>
2023-07-05 23:22:19 -0300

    modificacion para layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page: any) => page)
+  // return <Component {...pageProps} />
+  return getLayout(<Component {...pageProps} />)
 }
