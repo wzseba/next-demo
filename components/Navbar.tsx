e4a104f0a84364ec97611ac4b8ef0287f3815e27
wzseba <wzseba@gmail.com>
2023-07-05 23:04:23 -0300

    se agrego navegacion dinamica

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,36 @@
 import {ActiveLink} from './ActiveLink'
 import style from './Navbar.module.css'
 
+const menuItems = [
+  {
+      text: 'Home',
+      href: '/'
+  },
+  {
+      text: 'About',
+      href: '/about'
+  },
+  {
+      text: 'Contact',
+      href: '/contact'
+  },
+  {
+      text: 'Pricing',
+      href: '/pricing'
+  },
+];
+
 export const Navbar = () => {
   return (
     <nav className={style['menu-container']}>
-        <ActiveLink text='Home' href="/"/>
+        {/* <ActiveLink text='Home' href="/"/>
         <ActiveLink text='About' href="/about"/>
-        <ActiveLink text='Contact' href="/contact"/>
+        <ActiveLink text='Contact' href="/contact"/> */}
+        {
+          menuItems.map(({text, href}) => (
+            <ActiveLink key={text} text={text} href={href}/>
+          ))
+        }
     </nav>
   )
 }
